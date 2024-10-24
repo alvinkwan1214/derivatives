e65fee5f577fbe62f850275f25399285291ec77a
alvinkwan1214 <alvink12142000@gmail.com>
2024-10-24 14:19:56 +0100

    made changes

--- calculations.py
+++ calculations.py
@@ -2,14 +2,14 @@ from derivatives import eu_option, futures, normal_stock
 import numpy as np
 import matplotlib.pyplot as plt 
 
-underlying_range = np.linspace(20, 60, 41)
+underlying_range = np.linspace(0, 60, 61)
 payout_call = eu_option(50, underlying_range, option_type="Call", position='short', cost=5)
 
 payout_put = eu_option(30, underlying_range, option_type="Put", position='long', cost=7)
 
 payout_stock = normal_stock(40, underlying_range)
 
-total = (payout_put + payout_stock)
+total = 200*payout_put + 100*payout_stock + 200*payout_call
 
 plt.plot(underlying_range, total)
 plt.axhline(0, color='black', linewidth=1)
